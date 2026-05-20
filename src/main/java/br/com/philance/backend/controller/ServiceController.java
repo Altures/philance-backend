da7e6e87c60cc2e1edf5e09a3d0acd13067cf891
Altures <antoniomanduca15@gmail.com>
2026-05-20 17:24:15 -0300

    Pastas Config e Controler

---
+++ src/main/java/br/com/philance/backend/controller/ServiceController.java
@@ -0,0 +1,14 @@
+package br.com.philance.backend.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+
+
+@RestController
+public class ServiceController {
+}
