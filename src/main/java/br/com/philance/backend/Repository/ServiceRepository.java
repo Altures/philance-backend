64377e38334d47aeafbacab585ea726e3601f672
Altures <antoniomanduca15@gmail.com>
2026-05-22 14:31:12 -0300

    fixed last bugs

--- src/main/java/br/com/philance/backend/Repository/ServiceRepository.java
+++ src/main/java/br/com/philance/backend/Repository/ServiceRepository.java
@@ -1,9 +1,8 @@
 package br.com.philance.backend.Repository;
 
+import br.com.philance.backend.model.Service;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-public interface ServiceRepository {
-    @Repository
-    public interface ServicoRepository extends JpaRepository<Service, Long>{}
-}
+@Repository
+public interface ServiceRepository extends JpaRepository<Service, Long>{}
