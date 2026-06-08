cfa02abb109fb69fe3a68bf8e70c4f25d4b54f6b
Antônio Manduca <antoniomanduca15@gmail.com>
2026-06-08 17:11:17 -0300

    Atualização de Assignment

--- src/main/java/br/com/philance/backend/Repository/AssignmentRepository.java
+++ src/main/java/br/com/philance/backend/Repository/AssignmentRepository.java
@@ -2,7 +2,13 @@ package br.com.philance.backend.Repository;
 
 import br.com.philance.backend.model.Assignment;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface AssignmentRepository extends JpaRepository<Assignment, Long>{}
+public interface AssignmentRepository extends JpaRepository<Assignment, Long>{
+    @Query(value = "SELECT * FROM sua_tabela ORDER BY RAND() LIMIT 1", nativeQuery = true)
+    Optional<Assignment> findRandomAssignment();
+}
