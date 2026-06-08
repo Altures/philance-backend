51149b0df220185c1bcdae3b50a26a28f3b2c7d6
Antônio Manduca <antoniomanduca15@gmail.com>
2026-06-08 17:12:32 -0300

    Add files via upload

---
+++ src/main/java/br/com/philance/backend/DTO/response/AssignmentForLibraryDTO.java
@@ -0,0 +1,25 @@
+package br.com.philance.backend.DTO.response;
+
+import br.com.philance.backend.model.Assignment;
+
+public record AssignmentForLibraryDTO(
+        String title,
+        String company,
+        String Address,
+        String description,
+        Double payment,
+        int min_age,
+        String attire
+) {
+    public AssignmentForLibraryDTO (Assignment assignment){
+        this(
+                assignment.getTitle(),
+                assignment.getCompany() !=null?assignment.getCompany().getUsername(): null,
+                assignment.getAddress() !=null? assignment.getAddress().getCity(): null,
+                assignment.getDescription(),
+                assignment.getPayment(),
+                assignment.getMin_age(),
+                assignment.getAttire()
+            );
+    }
+}
