5b16e0c0b81226ca9da899b266e2fb63b785108c
Antônio Manduca <antoniomanduca15@gmail.com>
2026-06-10 17:20:30 -0300

    User Login

--- src/main/java/br/com/philance/backend/DTO/request/rating/PublishReviewDTO.java
+++ src/main/java/br/com/philance/backend/DTO/request/rating/PublishReviewDTO.java
@@ -1,7 +1,7 @@
-package br.com.philance.backend.DTO.request.rating;
+package br.com.philance.backend.DTO;
 
 public record PublishReviewDTO(Long id_assignment,
                                Long id_author,
                                Long id_subject,
-                               int review,
+                               Integer review,
                                String comments){}
