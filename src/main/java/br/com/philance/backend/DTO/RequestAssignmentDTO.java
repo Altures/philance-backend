3d83020c450b6a689d854cbe6297c979eebf1887
Milena Novka <103338752+MilenaNovka@users.noreply.github.com>
2026-06-11 13:54:37 -0300

    Delete src/main/java/br/com/philance/backend/DTO/RequestAssignmentDTO.java

--- src/main/java/br/com/philance/backend/DTO/RequestAssignmentDTO.java
+++
@@ -1,9 +0,0 @@
-package br.com.philance.backend.DTO;
-
-public record RequestAssignmentDTO(Long id_company,
-                                   Long id_address,
-                                   String title,
-                                   String description,
-                                   Double payment,
-                                   int min_age,
-                                   String attire) {}
