abc945529e288bd0351c817c081279e3edd5ec14
Milena Novka <103338752+MilenaNovka@users.noreply.github.com>
2026-06-11 13:54:06 -0300

    Delete src/main/java/br/com/philance/backend/DTO/EditProfileDTO.java

--- src/main/java/br/com/philance/backend/DTO/EditProfileDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.philance.backend.DTO;
-
-public record EditProfileDTO(Long id_address,
-                             String username,
-                             String phone,
-                             String description,
-                             String password) { }
