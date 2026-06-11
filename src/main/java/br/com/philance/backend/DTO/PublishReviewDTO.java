5b350e9b877855ad9666a952bd1a62fa3ca076f8
Milena Novka <103338752+MilenaNovka@users.noreply.github.com>
2026-06-11 13:54:22 -0300

    Delete src/main/java/br/com/philance/backend/DTO/PublishReviewDTO.java

--- src/main/java/br/com/philance/backend/DTO/PublishReviewDTO.java
+++
@@ -1,7 +0,0 @@
-package br.com.philance.backend.DTO;
-
-public record PublishReviewDTO(Long id_assignment,
-                               Long id_author,
-                               Long id_subject,
-                               int review,
-                               String comments){}
